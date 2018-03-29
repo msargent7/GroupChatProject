7d59deff73410246a87a7ded2ecfd0555d8ae5f2
Tom <NHTI@L230-08>
2018-03-29 15:00:57 -0400

    start of project

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,17 @@
-//hello
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+
+// Link with ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
+
+#include <iostream>
+using namespace std;
+
+const unsigned short PORT = 59202;
+const int BUF_SIZE = 64;
+
+int main()
+{
+  return 0;
+}
